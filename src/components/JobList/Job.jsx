4bfc1cf4b00a082e4034b5031d9bca07933ca23f
Tanjil Hossain <tanjilhossain845.com>
2023-09-22 15:33:04 +0600

    compleate show all job funtionality

--- src/components/JobList/Job.jsx
+++ src/components/JobList/Job.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
-const Job = () => {
+const Job = ({ job }) => {
+  const { id, type, title, salary, deadline } = job || {};
   const navigate = useNavigate();
   const handelEdit = () => {
     navigate("/edit-job");
@@ -8,20 +9,28 @@ const Job = () => {
   return (
     <div className="lws-single-job">
       <div className="flex-1 min-w-0">
-        <h2 className="lws-title">Back End Developer</h2>
+        <h2 className="lws-title">{title}</h2>
         <div className="job-footers">
           <div className="lws-type">
             {/* <!-- Fulltime - #FF8A00,  --><!-- Internship - #FF5757,  --><!-- Remote - #56E5C4,  --> */}
-            <i className="fa-solid fa-stop !text-[#FF8A00] text-lg mr-1.5"></i>
-            Full-time
+            <i
+              className={`fa-solid fa-stop ${
+                type == "Internship"
+                  ? "!text-[#FF5757]"
+                  : type == "Remote"
+                  ? "!text-[#56E5C4]"
+                  : "!text-[#FF8A00]"
+              } text-lg mr-1.5`}
+            ></i>
+            {type}
           </div>
           <div className="lws-salary">
             <i className="fa-solid fa-bangladeshi-taka-sign text-slate-400 text-lg mr-1.5"></i>
-            BDT 40,000
+            BDT {salary}
           </div>
           <div className="lws-deadline">
             <i className="fa-regular fa-calendar text-slate-400 text-lg mr-1.5"></i>
-            Closing on 2022-12-31
+            Closing on {deadline}
           </div>
         </div>
       </div>
--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -1,10 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
 import Job from "./Job";
+import { useEffect } from "react";
+import { fetchJobs } from "../../redux/features/Job/jobSlice";
 
 const JobList = () => {
+  const { isLoading, isError, jobs, error } = useSelector(
+    (state) => state.jobs
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchJobs());
+  }, [dispatch]);
+
+  let content;
+  if (isLoading) content = <p>loading....</p>;
+  if (!isLoading && isError) content = <p className="error">{error}</p>;
+  if (!isError && !isLoading && jobs.length == 0)
+    content = <p>No jobs Available !</p>;
+  if (!isError && !isLoading && jobs.length > 0) {
+    content = jobs.map((job) => <Job key={job.id} job={job} />);
+  }
   return (
     <div className="jobs-list">
       {/* <!-- Single Job 1--> */}
-      <Job />
+      {content}
       {/* <!-- Single Job 1--> */}
     </div>
   );
